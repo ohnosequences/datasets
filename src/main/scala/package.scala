770944e9bb648caa21419403ce5003c02cd6a43a
Alexey Alekhin <laughedelic@gmail.com>
2017-10-03 19:57:38 +0200

    Fixed AnyDataset type definition

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -4,7 +4,7 @@ import ohnosequences.cosas._, types._, records._, klists._
 
 package object datasets {
 
-  type AnyDataSet = AnyRecordType { type Keys <: AnyProductType { type TypesBound <: AnyData } }
+  type AnyDataSet = AnyRecordType.Of[AnyData]
 
   type Resources[R <: AnyDataResource] = *[AnyDenotation { type Value <: R }]
   def  Resources[R <: AnyDataResource]: Resources[R] = *[AnyDenotation { type Value <: R }]
